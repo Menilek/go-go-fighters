1021f00606e05e13453917853dc068a56796960f
Menilek <menilek25@hotmail.co.uk>
2020-08-16 13:16:31 +0100

    Implemented routing using gorilla mux

--- main.go
+++ main.go
@@ -1,27 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"encoding/json"
+
+	"github.com/gorilla/mux"
 )
 
 type Fighter struct {
-	Name string `json:"name"`
+	Name      string `json:"name"`
 	Grappling string `json:"grappling"`
-	Striking string `json:"striking"`
+	Striking  string `json:"striking"`
 }
 
 type Fighters []Fighter
 
-func allFighters(w http.ResponseWriter, r *http.Request){
+func allFighters(w http.ResponseWriter, r *http.Request) {
 	fighters := Fighters{
 		Fighter{Name: "Menilek", Grappling: "Jiu Jitsu", Striking: "Muay Thai"},
 	}
 
 	fmt.Println("Top Fighters")
-	json.NewEncoder(w).Encode(fighters) 
+	json.NewEncoder(w).Encode(fighters)
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -29,9 +31,10 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/fighters", allFighters)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/fighters", allFighters).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
 func main() {
